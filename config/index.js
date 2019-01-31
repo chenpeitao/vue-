40cca6fa28684dcdc627b2543b68029df7515512
chenpeitao <844952631@qq.com>
2019-01-31 10:00:14 +0800

    Update index.js
    打包

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/F4/vue_test/vue_test03/dist',
 
     /**
      * Source Maps
